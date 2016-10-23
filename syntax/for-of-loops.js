182d14db4ad97696e64f9b12d3e97250ba370663
Dave Herzog <dherzog@dramafever.com>
2016-10-23 11:32:26 -0400

    Object Literal Extensions

---
+++ syntax/for-of-loops.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  var categories = ['hardware', 'software', 'vaporware'];
+  for (var item of categories) {
+    console.log(item);
+  }
+})();
+
+(function () {
+  'use strict';
+
+  var categories = [, ,];
+  for (var item of categories) {
+    console.log(item);
+  }
+})();
+
+(function () {
+  'use strict';
+
+  var codes = "ABCDF";
+  var count = 0;
+  for (var code of codes) {
+  }
+  count++;
+  console.log(count);
+})();
