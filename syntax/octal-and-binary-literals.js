9c2967d757132f4ec1d17a2900a3221ee67ef219
Dave Herzog <dherzog@dramafever.com>
2016-10-23 11:36:20 -0400

    Octal and Binary literals

---
+++ syntax/octal-and-binary-literals.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  var value = 0o10;
+  console.log(value);
+})();
+
+(function () {
+  'use strict';
+
+  var value = 0O10;
+  console.log(value);
+})();
+
+(function () {
+  'use strict';
+
+  var value = 0b10;
+  console.log(value);
+})();
+
+(function () {
+  'use strict';
+
+  var value = 0B10;
+  console.log(value);
+})();
